5d9fde585e7e34cec05bffaf900d77704d3431b2
ThomasCanning <thomas500book@gmail.com>
2021-04-29 19:31:28 +0100

    GUI Menu Screen refactor

--- src/com/HangmanGame/GUI.java
+++ src/com/HangmanGame/GUI.java
@@ -1,15 +1,15 @@
-package com.HangmanGame.GUI;
+package com.HangmanGame;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Menu {
+public class GUI {
     private JButton Start;
     private JPanel MenuPanel;
 
-    public Menu() {
+    public GUI() {
         Start.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -21,7 +21,7 @@ public class Menu {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Menu");
         frame.setPreferredSize(new Dimension(600, 300));
-        frame.setContentPane(new Menu().MenuPanel);
+        frame.setContentPane(new GUI().MenuPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
